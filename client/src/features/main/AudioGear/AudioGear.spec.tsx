f9eefa7ee70ed1361003b1302e145c0189cf5919
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-02 18:17:20 +0200

    feat/AudioGear - improve unit tests

--- client/src/features/main/AudioGear/AudioGear.spec.tsx
+++ client/src/features/main/AudioGear/AudioGear.spec.tsx
@@ -2,12 +2,22 @@ import { screen } from "@testing-library/react";
 import { AudioGear } from "./index";
 import { render } from "../../../../tests/render";
 
-it("should render AudioGear component", async () => {
+it("displays title", async () => {
   render(<AudioGear />);
-  const img = screen.getByAltText("audio-gear-image");
+  expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+    "Bringing you the best audio gear"
+  );
+});
+
+it("displays description", async () => {
+  render(<AudioGear />);
   const description = screen.getByText(
     "Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
   );
-  expect(img).toBeInTheDocument();
   expect(description).toBeInTheDocument();
+});
+
+it("displays img", async () => {
+  render(<AudioGear />);
+  expect(screen.getByAltText("audio-gear-image")).toBeInTheDocument();
 });
