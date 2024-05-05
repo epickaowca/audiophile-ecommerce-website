00853c971e77cd0f365200c044a1bf00d979a391
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-05 09:11:25 +0200

    feat/Checkout - fix form unit tests

--- client/src/features/main/Checkout/Checkout.spec.tsx
+++ client/src/features/main/Checkout/Checkout.spec.tsx
@@ -1,6 +1,8 @@
 import { render } from "../../../../tests/render";
 import { screen, waitFor } from "@testing-library/react";
 import { Checkout } from "./index";
+import { Form } from "./components/Form";
+const MockedForm = Form as jest.Mock<any>;
 
 jest.mock("./components/ThankYou", () => ({
   ThankYou: jest.fn(() => <h1>Thank You</h1>),
@@ -15,17 +17,21 @@ jest.mock("../Cart", () => ({
   )),
 }));
 
+jest.mock("./components/Form", () => ({
+  Form: jest.fn(() => <h1>Form</h1>),
+}));
+
 it("displays Thank You component after form submit", async () => {
-  jest.mock("./components/Form", () => ({
-    Form: jest.fn(() => <h1>Form</h1>),
-  }));
-  const { rerender } = render(<Checkout />);
+  render(<Checkout />);
   await screen.getByText("submit").click();
-  rerender(<Checkout />);
-  expect(screen.getByText("Thank You")).toBeInTheDocument();
+
+  await waitFor(async () => {
+    expect(screen.getByText("Thank You")).toBeInTheDocument();
+  });
 });
 
 it("does not submit form if required inputs are empty", async () => {
+  MockedForm.mockReturnValue(<input required />);
   const { rerender } = render(<Checkout />);
   await screen.getByText("submit").click();
   rerender(<Checkout />);
