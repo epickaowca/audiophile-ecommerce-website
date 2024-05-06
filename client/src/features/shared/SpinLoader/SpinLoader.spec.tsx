3351a3b00dbf2fe47fdc7513f364a7e1f9955786
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-06 16:07:57 +0200

    feat/SpinLoader - update unit tests

--- client/src/features/shared/SpinLoader/SpinLoader.spec.tsx
+++ client/src/features/shared/SpinLoader/SpinLoader.spec.tsx
@@ -2,9 +2,7 @@ import { screen } from "@testing-library/react";
 import { SpinLoader } from "./index";
 import { render } from "../../../../tests/render";
 
-it("should render spinLoader with correct className", async () => {
-  render(<SpinLoader height="500px" />);
-  const div = screen.getByLabelText("spin-loader");
-  expect(div).toBeInTheDocument();
-  expect(div).toHaveClass("spin-loader");
+it("displays spinLoader", async () => {
+  render(<SpinLoader />);
+  expect(screen.getByLabelText("Loading...")).toBeInTheDocument();
 });
