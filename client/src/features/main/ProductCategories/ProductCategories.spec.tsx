13dcb3b38c4aaeb4c8e519df2f552ef796d91c6f
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-09 09:43:15 +0200

    feat/ProductCategories - update unit tests

--- client/src/features/main/ProductCategories/ProductCategories.spec.tsx
+++ client/src/features/main/ProductCategories/ProductCategories.spec.tsx
@@ -2,28 +2,47 @@ import { screen } from "@testing-library/react";
 import { render } from "@tests/render";
 import { ProductCategories } from "./index";
 
+const ProductProps = jest.fn();
 jest.mock("./components/Product", () => ({
-  Product: jest.fn(({ autoFocus }) => (
-    <>
-      <h1>Product</h1>
-      <h1>{`autoFocus:${autoFocus}`}</h1>
-    </>
-  )),
+  Product: jest.fn((props) => {
+    ProductProps(props);
+    return <div data-testId="Product"></div>;
+  }),
 }));
 
 it("displays Products", () => {
   render(<ProductCategories />);
-  expect(screen.getAllByText("Product")).toHaveLength(3);
+  expect(screen.getAllByTestId("Product")).toHaveLength(3);
 });
 
-it("displays first Products focused", () => {
+it("displays Product with props", () => {
   render(<ProductCategories autoFocus />);
-  expect(screen.getAllByText(`autoFocus:true`)).toHaveLength(1);
+  expect(ProductProps).toHaveBeenCalledWith(
+    expect.objectContaining({
+      href: "/category/headphones",
+      productName: "headphones",
+    })
+  );
+  expect(ProductProps).toHaveBeenCalledWith(
+    expect.objectContaining({
+      href: "/category/speakers",
+      productName: "speakers",
+    })
+  );
+  expect(ProductProps).toHaveBeenCalledWith(
+    expect.objectContaining({
+      href: "/category/earphones",
+      productName: "earphones",
+    })
+  );
 });
 
-it("displays first Products focused", () => {
+it("displays first Product focused", () => {
   render(<ProductCategories autoFocus />);
-  expect(screen.getAllByText(`autoFocus:true`)).toHaveLength(1);
+  expect(ProductProps).toHaveBeenNthCalledWith(
+    1,
+    expect.objectContaining({ autoFocus: true })
+  );
 });
 
 it("displays as menuBar", () => {
